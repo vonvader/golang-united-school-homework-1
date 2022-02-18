8dc8a364f1c8a07c15276370bde6cc99bce4b29e
Alexander Oplesnin <aoplesnin@Alexs-MBP.local>
2022-02-17 22:50:33 -0800

    Initial commit for homework 1

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kyokomi/emoji"
+)
+
+func GetMessage() string {
+
+	return emoji.Sprint("Hello :world_map:!")
+
+}
+
+func main() {
+	fmt.Print(GetMessage())
+}
